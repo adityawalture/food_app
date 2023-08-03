607547d5e6957f65c7c3beccfa422a6bade832a6
adi_walture <adityawalture9298@gmail.com>
2023-08-04 02:14:27 +0530

    WillPopScope added

--- lib/Pages/tabs.dart
+++ lib/Pages/tabs.dart
@@ -50,14 +50,16 @@ class _TabsScreenState extends State<TabsScreen> {
   }
 
   //render display by selecting from drawer list
-  void _setScreen(String identifier) {
+  void _setScreen(String identifier) async {
     Navigator.of(context).pop();
     if (identifier == 'filters') {
-      Navigator.of(context).push(
+      final result = await Navigator.of(context).push<Map<Filter, bool>>(
         MaterialPageRoute(
           builder: (ctx) => const FiltersScreen(),
         ),
       );
+
+      // print(result);
     }
   }
 
